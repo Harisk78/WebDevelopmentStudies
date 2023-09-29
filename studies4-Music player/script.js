eea679a0cd0f05bdf2db2151d854af2a053192c7
HARIHARASUDHAN SK <harisksk78@gmail.com>
2023-09-29 12:02:29 +0530

    music player added

--- studies4-Music player/script.js
+++ studies4-Music player/script.js
@@ -1,15 +1,22 @@
+//GETTING FROM HTML
+
 let aname=document.getElementById('name');
 let aartist=document.getElementById('artist');
 let aimg=document.getElementById('img');
 let slide=document.getElementById('inp');
-
+let song_duration=document.getElementById('duration');
+let song_tduration=document.getElementById('tduration');
+let slide_position;
 let play=document.getElementById("play");
+let c=0,x=0;
 
-let c=0;
+//CREATING AUDIO
 
 let track_song=document.createElement('audio');
 track_song.src='song/song1.mp3';
 aimg.src='img/spb.jpg';
+
+//AUDIO DETAILS
 
 let index=0;
 let song_list=[
@@ -32,18 +39,77 @@ let song_list=[
         img:'img/karthik.jpg'
     }
 ]
+// DURATION
+track_song.addEventListener("timeupdate",dur);
+
+//CHANGING DURATION BY RANGE SLIDER
+
+function change_duration(){
+    slide_position=track_song.duration*(slide.value/100);
+    track_song.currentTime = slide_position;
+}
+
+// SONG DURATION AND CURRENT DURATION
+
+function dur(){
+    let time ;
+    if(track_song.duration){
+        let min = Math.floor(track_song.currentTime/60);
+        let sec = Math.floor(track_song.currentTime - min * 60);
+
+        let tmin = Math.floor(track_song.duration/60);
+        let tsec = Math.floor(track_song.duration - tmin * 60);
+
+        if(min<10){
+            min = '0' + min;
+        }
+        if(sec<10){
+            sec = '0' + sec;
+        }
+        if(tmin<10){
+            tmin = '0' + tmin;
+        }
+        if(tsec<10){
+            tsec = '0' + tsec;
+        }
+        time = min + ':' + sec;
+        song_tduration.innerHTML= tmin + ':' + tsec;
+    }
+    else{
+        time = '00' + ':' + '00';
+    }
+    song_duration.innerHTML =time;
+    
+    if(track_song.currentTime === track_song.duration){
+        next_song();
+    }
+    
+}
+
+let a=1;
+function rangeslider(){
+    slide.value=a;
+    a+=1;
+}
+
+// PLAYING SONG
+
+let interval;
 function play_song(){
     c++;
     if(c%2!=0){
         track_song.play();
         play.innerHTML='<ion-icon name="pause-outline"></ion-icon>'
+        interval = setInterval(rangeslider,1000);
     }
     else{
         play.innerHTML='<ion-icon name="play-outline"></ion-icon>'
         track_song.pause();
+        clearInterval(interval);
     }
 }
 
+//NEXT SONG
 
 function next_song(){
     c=1;
@@ -60,6 +126,8 @@ function next_song(){
     aimg.src=song_list[index].img;
     slide.value=0;
 }
+
+//PREVIOUS SONG
 
 function prev_song(){
     c=1;
@@ -79,10 +147,3 @@ function prev_song(){
     aimg.src=song_list[index].img;
     slide.value=0;
 }
-
-function change_duration(){
-    slide_position=track_song.duration*(slide.value/100);
-    track_song.currentTime = slide_position;
-}
-
-
